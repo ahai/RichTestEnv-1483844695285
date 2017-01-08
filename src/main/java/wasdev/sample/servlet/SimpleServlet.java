4e36ecce30ccc0205ae2eafcd1521f8557278f5d
ahai <seamorning@gmail.com>
2017-01-07 23:04:15 -0500

    this is a good sample.

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class SimpleServlet
  */
-@WebServlet("/SimpleServlet")
+@WebServlet("/SimpleServlet1")
 public class SimpleServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
